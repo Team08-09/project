9ccf08ff50d984edbe0457e789dbe364705b31be
asila2104 <79084814+asila2104@users.noreply.github.com>
2021-10-05 16:38:04 +0300

    appBar and bottomNavigationBar

---
+++ android/app/src/main/kotlin/com/example/vestes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vestes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF000000);
+const kIconsColor = Color(0xFFC5C5C5);
+const kTextLightColor = Color(0xFFACACAC);
+const MainPurple = Color(0x5B00046D);
+
+const kDefaultPaddin = 20.0;
